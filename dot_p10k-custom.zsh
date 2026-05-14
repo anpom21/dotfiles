2720fc77fc63e1ffe95f99f5855bbd562d830a4f
Andreas Pommerencke <anpom21@student.sdu.dk>
2026-05-14 07:29:29 +0200

    p10k custom update

--- dot_p10k-custom.zsh
+++ dot_p10k-custom.zsh
@@ -1,5 +1,8 @@
 typeset -g POWERLEVEL9K_VIRTUALENV_VISUAL_IDENTIFIER_EXPANSION=''
 typeset -g POWERLEVEL9K_VIRTUALENV_CONTENT_EXPANSION='(${P9K_CONTENT})'
+typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
+typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
 
 function my_git_formatter() {
   emulate -L zsh
@@ -10,15 +13,16 @@ function my_git_formatter() {
   fi
 
   local branch
+  local git_ref_color='%F{#F14C4C}'
 
   if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
     branch=${(V)VCS_STATUS_LOCAL_BRANCH}
-    typeset -g my_git_format="%B%196F[${branch//\%/%%}]%f%b"
+    typeset -g my_git_format="%B${git_ref_color}[${branch//\%/%%}]%f%b"
   elif [[ -n $VCS_STATUS_TAG ]]; then
     local tag=${(V)VCS_STATUS_TAG}
-    typeset -g my_git_format="%B%196F[#${tag//\%/%%}]%f%b"
+    typeset -g my_git_format="%B${git_ref_color}[#${tag//\%/%%}]%f%b"
   else
-    typeset -g my_git_format="%B%196F[@${VCS_STATUS_COMMIT[1,8]}]%f%b"
+    typeset -g my_git_format="%B${git_ref_color}[@${VCS_STATUS_COMMIT[1,8]}]%f%b"
   fi
 }
 functions -M my_git_formatter 2>/dev/null
